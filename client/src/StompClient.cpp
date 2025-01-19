7bccf7977f85d8a465fae8a1ad3a98685a8db62e
OmerSetty <74535151+OmerSetty@users.noreply.github.com>
2025-01-19 19:44:56 +0000

    Initializing StompClient basic threads structure

--- client/src/StompClient.cpp
+++ client/src/StompClient.cpp
@@ -1,3 +1,24 @@
+class ThreadsHandler {
+public:
+    
+	
+    void foo()
+    {
+        cout << "Thread using non-static member function "
+                "as callable"
+             << endl;
+    }
+    // static member function
+    static void foo1()
+    {
+        cout << "Thread using static member function as "
+                "callable"
+             << endl;
+    }
+};
+
+
+
 int main(int argc, char *argv[]) {
 	// TODO: implement the STOMP client
 	return 0;

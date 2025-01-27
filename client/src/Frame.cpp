95a981e056d2b9a532dcbf0ca6a46fcbad3b5084
MrKouch <guykou@post.bgu.ac.il>
2025-01-27 21:00:06 +0000

    running server with client

--- client/src/Frame.cpp
+++ client/src/Frame.cpp
@@ -49,7 +49,6 @@ SendFrame::SendFrame(const string& destination, Event& event)
                 "general_information:" + "\n" +
                 "active:" + event.get_general_information().at("active") + "\n" +
                 "forces_arrival_at_scene:" + event.get_general_information().at("forces_arrival_at_scene") + "\n";
-                cout << "[DEBUG] destination channel is: " << destination << endl;
     }
 
 UnsubscribeFrame::UnsubscribeFrame(const string& subID, int receiptID)
--- client/src/StrompProtocol.cpp
+++ client/src/StrompProtocol.cpp
@@ -333,6 +333,10 @@ string StompProtocol:: processConnected() {
 
 
 string StompProtocol:: processReceipt(vector<string> args) {
+    cout << "[DEBUG] RECEIPT IS:" << endl;
+    for (string arg : args) {
+        cout << arg << endl;
+    }
     vector<string> header = splitLine(args[1]);
     string receipt = header[1];
     try {
@@ -341,13 +345,7 @@ string StompProtocol:: processReceipt(vector<string> args) {
         }
         int receiptID = std::stoi(receipt);
         if (receiptID == logOutReceiptID) {
-<<<<<<< HEAD
-            cout << "[DEBUG] RECEIPT IS:" << endl;
             for(string arg : args) {
-=======
-            cout << "[DEBUG] RECEIPT IS: " << endl;
-            for (string arg : args) {
->>>>>>> 5788d440a38100bc815b2b216e46e4adeff8109f
                 cout << arg << endl;
             }
             disconnect();
@@ -355,7 +353,7 @@ string StompProtocol:: processReceipt(vector<string> args) {
         }
         // Join a channel
         else if (receiptID == joinChannelReceiptID) {
-            cout << "[DEBUG] Joining channel" << endl;
+            cout << "[DEBUG] Joining channel. RECEIPT IS:" << endl;
             for (string arg : args) {
                 cout << arg << endl;
             }
@@ -406,6 +404,9 @@ vector<string> StompProtocol :: splitLine(const string& line) {
         }
     }
     args.push_back(word);
+    for (string arg : args) {
+        cout << arg << endl;
+    }
     return args;
 }
 
